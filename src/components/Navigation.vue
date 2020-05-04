9b038293575ed7f95a8f0b5a0049cf63dff4a98f
Park, Sungmin <spark323@gmail.com>
2020-05-04 12:48:28 +0900

    added Firebase and BootstrapVue

---
+++ src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Home</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/home" v-if="user">home</router-link>
+          <router-link class="nav-item nav-link" to="/login" v-if="!user">log in</router-link>
+          <router-link class="nav-item nav-link" to="/register" v-if="!user">register</router-link>
+          <button class="nav-item nav-link btn btn-link" @click="$emit('logout')">logout</button>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
