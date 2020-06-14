eed6bd7be7f0a2c81ab56875ff8454ab7b495465
Jilly Moon <moonji@gmail.com>
2020-06-14 19:05:15 +0900

    Fix #51 #48 #44 resize avatar and bugs

--- src/store/modules/auth.module.js
+++ src/store/modules/auth.module.js
@@ -140,23 +140,19 @@ const actions = {
   },
   updateProfile: async ({ dispatch }, profile) => {
     try {
+      // copy userInfo to new userInfo
       const newUserInfo = state.userData.userInfo;
 
-      console.log('Type of profile.avatar: ', typeof profile.avatar);
-      console.log('profile.avatar: ', profile.avatar);
+      // console.log('Type of profile.avatar: ', typeof profile.avatar);
+      // console.log('profile.avatar: ', profile.avatar);
 
+      // copy new username
       newUserInfo.username = profile.username;
 
       // when the user removed the avatar
-      if (profile.avatar == null) {
-        console.log('user removed avatar and profile.avatar is null');
-        console.log(state.userData.userInfo.uid);
-
-        // remove avatar link from userCollection
-        const avatarLinkRef = usersCollection.doc(state.userData.userInfo.uid);
-        await avatarLinkRef.update({
-          avatar: '',
-        });
+      if (newUserInfo.avatar != '' && profile.avatar == '') {
+        // console.log('user removed avatar');
+        // console.log(state.userData.userInfo.uid);
 
         // remove avatar from FB Storage
         const avatarRef = storage.ref('avatar/' + state.userData.userInfo.uid);
@@ -164,115 +160,167 @@ const actions = {
           await avatarRef.delete();
         }
 
-        state.userData.userInfo.avatar = '';
-      } else if (profile.avatar != state.userData.userInfo.avatar) {
-        // if avatar is changed
-        console.log('=== avatar is changed ===');
+        // remove avatar from new userInfo
+        newUserInfo.avatar = '';
+      }
 
-        console.log('Type of userData.userInfo.avatar): ', typeof state.userData.userInfo.avatar);
-        console.log('userData.userInfo.avatar: ', state.userData.userInfo.avatar);
-        let uploadTask = storage.ref('avatar/' + firebaseAuth.currentUser.uid).put(profile.avatar);
+      // if avatar is changed (not deleted)
+      if (profile.image != null) {
+        // console.log('=== avatar is changed ===');
+        // console.log('Type of userData.userInfo.avatar): ', typeof state.userData.userInfo.avatar);
+        // console.log('userData.userInfo.avatar: ', state.userData.userInfo.avatar);
 
-        // Listen for state changes, errors, and completion of the upload.
-        uploadTask.on(
-          'state_changed',
-          function(snapshot) {
-            switch (snapshot.state) {
-              case 'paused':
-                break;
-              case 'running':
-                break;
+        const reader = new FileReader();
+        reader.readAsDataURL(profile.image);
+        reader.onload = function(e) {
+          let image = new Image();
+          image.src = e.target.result;
+          image.onload = function() {
+            let width = this.width;
+            let height = this.height;
+            const max_width = 140;
+            const max_height = 140;
+
+            // if image is bigger than 140x140, resize the image
+            // if (width > max_width || height > max_height) {
+
+            // calculate the width and height, constraining the proportions
+            if (width > height) {
+              if (width > max_width) {
+                height = Math.round((height *= max_width / width));
+                width = max_width;
+              }
+            } else {
+              if (height > max_height) {
+                width = Math.round((width *= max_height / height));
+                height = max_height;
+              }
             }
-          },
-          function(error) {
-            switch (error.code) {
-              case 'storage/unauthorized':
-                console.log(error);
-                break;
-              case 'storage/canceled':
-                console.log(error);
-                break;
-              case 'storage/unknown':
-                console.log(error);
-                break;
-            }
-          },
-          function() {
-            // Upload completed successfully, now we can get the download URL
-            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-              newUserInfo.avatar = downloadURL;
 
-              // Change username in firebaseAuth (displayName)
-              firebaseAuth.currentUser.updateProfile({
-                displayName: newUserInfo.username,
-              });
+            let canvas = document.createElement('canvas');
 
-              // Change username and avatar in usersCollection
-              usersCollection.doc(firebaseAuth.currentUser.uid).update({
-                username: newUserInfo.username,
-                avatar: newUserInfo.avatar,
-              });
+            // resize the canvas and draw the image data into it
+            canvas.width = width;
+            canvas.height = height;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0, width, height);
 
-              // Change username and avatar in allPostsCollection
-              allPostsCollection
-                .where('ownerId', '==', newUserInfo.uid)
-                .get()
-                .then(function(querySnapshot) {
-                  querySnapshot.forEach(function(doc) {
-                    allPostsCollection.doc(doc.id).update({
-                      username: newUserInfo.username,
-                      avatar: newUserInfo.avatar,
-                    });
-                  });
-                });
+            ctx.canvas.toBlob(
+              function(blob) {
+                const file = new File([blob], 'filenamewillbeuid', {
+                  type: 'image/jpeg',
+                  lastModified: Date.now(),
+                }); //output image as a file, No support on IE
+                //console.log('file ======', file);
 
-              // Change username and avatar in myPostsCollection
-              myPostsCollection
-                .doc(newUserInfo.uid)
-                .collection('userPosts')
-                .get()
-                .then(function(querySnapshot) {
-                  querySnapshot.forEach(function(doc) {
-                    myPostsCollection
-                      .doc(newUserInfo.uid)
-                      .collection('userPosts')
-                      .doc(doc.id)
-                      .update({
+                let uploadTask = storage.ref('avatar/' + firebaseAuth.currentUser.uid).put(file);
+
+                // Listen for state changes, errors, and completion of the upload.
+                uploadTask.on(
+                  'state_changed',
+                  function(snapshot) {
+                    switch (snapshot.state) {
+                      case 'paused':
+                        break;
+                      case 'running':
+                        break;
+                    }
+                  },
+                  function(error) {
+                    switch (error.code) {
+                      case 'storage/unauthorized':
+                        console.log(error);
+                        break;
+                      case 'storage/canceled':
+                        console.log(error);
+                        break;
+                      case 'storage/unknown':
+                        console.log(error);
+                        break;
+                    }
+                  },
+                  function() {
+                    // Upload completed successfully, now we can get the download URL
+                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                      newUserInfo.avatar = downloadURL;
+
+                      // Change username in firebaseAuth (displayName)
+                      firebaseAuth.currentUser.updateProfile({
+                        displayName: newUserInfo.username,
+                      });
+
+                      // Change username and avatar in usersCollection
+                      usersCollection.doc(firebaseAuth.currentUser.uid).update({
                         username: newUserInfo.username,
                         avatar: newUserInfo.avatar,
                       });
-                  });
-                });
 
-              // Change username and avatar in likesCollection
-              usersCollection.get().then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                  likesCollection
-                    .doc(doc.id)
-                    .collection('myLikes')
-                    .where('ownerId', '==', newUserInfo.uid)
-                    .get()
-                    .then(function(querySnapshot2) {
-                      querySnapshot2.forEach(function(doc2) {
-                        likesCollection
-                          .doc(doc.id)
-                          .collection('myLikes')
-                          .doc(doc2.id)
-                          .update({
-                            username: newUserInfo.username,
-                            avatar: newUserInfo.avatar,
+                      // Change username and avatar in allPostsCollection
+                      allPostsCollection
+                        .where('ownerId', '==', newUserInfo.uid)
+                        .get()
+                        .then(function(querySnapshot) {
+                          querySnapshot.forEach(function(doc) {
+                            allPostsCollection.doc(doc.id).update({
+                              username: newUserInfo.username,
+                              avatar: newUserInfo.avatar,
+                            });
                           });
+                        });
+
+                      // Change username and avatar in myPostsCollection
+                      myPostsCollection
+                        .doc(newUserInfo.uid)
+                        .collection('userPosts')
+                        .get()
+                        .then(function(querySnapshot) {
+                          querySnapshot.forEach(function(doc) {
+                            myPostsCollection
+                              .doc(newUserInfo.uid)
+                              .collection('userPosts')
+                              .doc(doc.id)
+                              .update({
+                                username: newUserInfo.username,
+                                avatar: newUserInfo.avatar,
+                              });
+                          });
+                        });
+
+                      // Change username and avatar in likesCollection
+                      usersCollection.get().then(function(querySnapshot) {
+                        querySnapshot.forEach(function(doc) {
+                          likesCollection
+                            .doc(doc.id)
+                            .collection('myLikes')
+                            .where('ownerId', '==', newUserInfo.uid)
+                            .get()
+                            .then(function(querySnapshot2) {
+                              querySnapshot2.forEach(function(doc2) {
+                                likesCollection
+                                  .doc(doc.id)
+                                  .collection('myLikes')
+                                  .doc(doc2.id)
+                                  .update({
+                                    username: newUserInfo.username,
+                                    avatar: newUserInfo.avatar,
+                                  });
+                              });
+                            });
+                        });
                       });
+                      dispatch('fetchUserProfile');
                     });
-                });
-              });
-              dispatch('fetchUserProfile');
-            });
-          }
-        );
+                  }
+                );
+              },
+              'image/jpeg',
+              0.9
+            );
+          };
+        };
       } else {
+        // console.log('=== avatar not changed (can be deleted) ===');
         // Change username in firebaseAuth (displayName)
-        console.log('=== avatar not changed ===');
         await firebaseAuth.currentUser.updateProfile({
           displayName: newUserInfo.username,
         });
@@ -280,6 +328,7 @@ const actions = {
         // Change username and avatar in usersCollection
         await usersCollection.doc(firebaseAuth.currentUser.uid).update({
           username: newUserInfo.username,
+          avatar: newUserInfo.avatar,
         });
 
         // Change username and avatar in allPostsCollection
@@ -290,6 +339,7 @@ const actions = {
             querySnapshot.forEach(function(doc) {
               allPostsCollection.doc(doc.id).update({
                 username: newUserInfo.username,
+                avatar: newUserInfo.avatar,
               });
             });
           });
@@ -307,6 +357,7 @@ const actions = {
                 .doc(doc.id)
                 .update({
                   username: newUserInfo.username,
+                  avatar: newUserInfo.avatar,
                 });
             });
           });
@@ -327,6 +378,7 @@ const actions = {
                     .doc(doc2.id)
                     .update({
                       username: newUserInfo.username,
+                      avatar: newUserInfo.avatar,
                     });
                 });
               });
